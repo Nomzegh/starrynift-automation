580eb9477bd1e970a58e4dd4a31bb0c4c3816a6e
Anatolii <101575015+Nomzegh@users.noreply.github.com>
2024-03-11 20:05:47 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+RPC = "https://rpc.ankr.com/bsc"  # другие RPC могут не принять газпрайс ниже 3 gwei
+
+
+id_to_follow = "EmOkC_Bp2z"  # ID кого нужно зафолловить (My profile -> share -> @id)
+transactions_break_time = 3  # перерыв между транзакциями
+gas_price = 1.01  # gasPrice в gwei. Не ставить < 1
